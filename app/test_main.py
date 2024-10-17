f5cb2ede9aee88c9266cf8c5627bec953cdd53cd
Alex <delete.rap.stupid@gmail.com>
2024-10-17 14:44:43 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+
+        ("", True),
+        ("1", True),
+        ("11", False),
+        ("Analogy", False),
+        ("#%*@)_#", False),
+        ("python", True),
+        ("  ", False),
+    ],
+    ids=[
+        "empty_string_false",
+        "one_char_True",
+        "two_same_chars_False",
+        "upper_letter_equal_to_lower_false",
+        "2_repeated_symbols_false",
+        "without_repeat_true",
+        "2_spaces_false",
+    ]
+)
+def test_is_isogram(string: str, expected: list) -> None:
+    assert is_isogram(string) == expected, (
+        f"Function 'is_isogram' should return {expected} "
+        f"when string is {string}"
+    )
